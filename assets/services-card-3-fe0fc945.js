b645fbce2d4e5b5ece35b7214c9fb0bd70f8f876
ArtyomShevchenko <artyomshevchenkowarmgray@gmail.com>
2023-12-13 16:05:49 +0200

    add folder /dist

---
+++ assets/services-card-3-fe0fc945.js
@@ -0,0 +1,1 @@
+const s="/edna/assets/services-card-3-4ab938e3.jpg";export{s as _};
---
+++ assets/user-3-7b2e0423.js
@@ -0,0 +1,1 @@
+const s="/edna/assets/user-1-c939a339.jpg",e="/edna/assets/user-2-39898dee.jpg",a="/edna/assets/user-3-54bc2dde.jpg";export{s as _,e as a,a as b};
